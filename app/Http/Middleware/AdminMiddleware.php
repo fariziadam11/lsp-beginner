b8f058e0b3503adb8e27d7b8bda274a62b705694
Firdaus <gunturdiawan@gmail.com>
2025-05-11 14:03:38 +0700

    fix error

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -10,9 +10,16 @@ class AdminMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
+        // Izinkan akses ke halaman login dan registrasi
+        if ($request->routeIs('filament.admin.auth.*')) {
+            return $next($request);
+        }
+
         if (Auth::check() && Auth::user()->role == 'admin') {
             return $next($request);
-        }        Auth::logout();
+        }
+        
+        Auth::logout();
         return redirect()->route('filament.admin.auth.login')->with('error', 'Anda tidak diizinkan mengakses halaman ini. Hanya admin yang dapat login.');
     }
 }
